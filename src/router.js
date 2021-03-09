af74e3bd5a7888a78f58fe3a97955a4eeeccc433
Tiago-devop <tiagodev@protonmail.com>
2021-03-09 15:43:14 -0300

    setup basic project and react router

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login">Login</Route>
+        <Route path="/register">register</Route>
+        <Route exact path="/">
+          Homepage
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
