5695f3ab7d1d9f3fbbec9d8c2ff549019ee0a78e
Tiago-devop <tiagodev@protonmail.com>
2021-03-09 16:12:32 -0300

    navbar component

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/misc/Navbar";
 
 function Router() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route path="/login">Login</Route>
         <Route path="/register">register</Route>
