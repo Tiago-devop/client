e4b5ca832bee1a0c2bed572a8a924f11f54d9039
Tiago-devop <tiagodev@protonmail.com>
2021-06-01 16:34:10 -0300

    setting up Dev and Prod environments for correct request URLs

--- src/components/home/Snippet.js
+++ src/components/home/Snippet.js
@@ -1,11 +1,12 @@
 import Axios from "axios";
 import React from "react";
+import domain from "../../util/domain";
 import "./Snippet.scss";
 
 function Snippet({ snippet, getSnippets, editSnippet }) {
   async function deleteSnippet() {
     if (window.confirm("Do you want to delete this snippet?")) {
-      await Axios.delete(`http://localhost:5000/snippet/${snippet._id}`);
+      await Axios.delete(`${domain}/snippet/${snippet._id}`);
 
       getSnippets();
     }
